9113910ceaae902dbf84a9b99b6162e97653e42d
hao <hao+github@iteaha.us>
2011-03-19 16:23:05 +0000

    first commit of erawan site, forked from mobile html5 boilerplate.

---
+++ README.markdown
@@ -0,0 +1,72 @@
+#Mobile Boilerplate (Beta) http://html5boilerplate.com
+v0.91 (code named Secret Diary)
+
+##Summary:
+
+This is a set of features made specifically for mobile development, with following included:
+
+###Markup:
+1. Home screen icon (Android, iOS, Symbian) (http://bit.ly/4bzrUX)
+2. CSS class target IE Mobile 7
+3. Cross browser viewport optimization (Android, iOS, Mobile IE, Blackberry) (http://bit.ly/c2b5GG)
+4. Optimized viewport scaling (Android, iOS, Mobile IE, Blackberry)
+4. IE Mobile better Font rendering
+5. Prevent scaling (http://bit.ly/blMIej)
+6. iPhone full screen mode (http://bit.ly/c2b5GG)
+7. SEO - avoid duplicated content
+
+###CSS:
+1. Mobile helper class
+2. Prevent text resize in ie/webkit browser
+3. Prevent callout
+4. HTML5 contenteditable attribute on mobile
+5. S60 3.x and 5.0 devices which animated gif fix
+6. Text overflow with ellipsis
+7. Mobile optimized default CSS
+
+###JavaScript:
+1. Touch/Gesture Events events (touchstart touchmove touchend touchcancel, gesturestart gesturechange gestureend)
+2. Orientationchange events (portrait landscape)
+3. Tap events (tap taphold)
+4. Swipe events (swipe swipeleft swiperight)
+5. Scroll events (scrollstart scrollstop)
+6. Cross browser CSS media queries
+7. Textarea autogrow (http://bit.ly/4D1NnN)
+8. Hide webkit chrome
+9. Load Stylesheet
+10. Firebug lite debugger
+11. Media queries for low end smartphone
+
+###Server:
+1. Added Blackberry MIME type for Apache, IIS server (http://bit.ly/gUBtIE)
+2. Added Nokia MIME type
+3. Prevent Transcoding
+4. Mobile site redirection
+
+###General:
+1. HTML5 offline caching for smartphone (http://bit.ly/1yqPo2)
+2. Mobile sitemap (http://bit.ly/uom43)
+3. Mobile bookmark bubble (http://bit.ly/b1dh6a)
+4. Browser Database Wrapper API (http://bit.ly/TJXll)
+5. User Agent Detection (http://bit.ly/9deoAd)
+6. GA for low end mobile devices (http://bit.ly/aMzwkO)
+7. Mobile build tool
+
+
+
+##License:
+
+###Major components:
+Respond.js: Dual licensed under the MIT or GPL Version 2 licenses<br />
+Bookmark bubble library: Apache License, Version 2.0<br />
+Web Storage Portability Layer: Apache License, Version 2.0<br />
+Modernizr: MIT/BSD license<br />
+jQuery: MIT/GPL license<br />
+DD_belatedPNG: MIT license<br />
+YUI Profiling: BSD license<br />
+HTML5Doctor CSS reset: Creative Commons 3.0 <br />
+CSS Reset Reloaded: Public Domain 
+
+###Everything else:
+
+The Unlicense (aka: public domain)
---
+++ js/mylibs/helper.js
@@ -0,0 +1,122 @@
+/*!
+ * mbp - Moible boilerplate helper functions: 
+ * http://www.blog.highub.com/
+ * by Shi Chuan
+ */
+ 
+// Hide URL Bar for iOS
+// http://remysharp.com/2010/08/05/doing-it-right-skipping-the-iphone-url-bar/
+function hideUrlBar() {
+	/mobile/i.test(navigator.userAgent) && !pageYOffset && !location.hash && setTimeout(function () {
+	window.scrollTo(0, 1);
+	}, 1000);
+}
+
+
+// Fast Buttons
+// http://code.google.com/mobile/articles/fast_buttons.html
+function fastButton(element, handler) {
+    this.element = element;
+    this.handler = handler;
+	element.addEventListener('touchstart', this, false);
+	element.addEventListener('click', this, false);
+};
+
+fastButton.prototype.handleEvent = function(event) {
+    switch (event.type) {
+		case 'touchstart': this.onTouchStart(event); break;
+        case 'touchmove': this.onTouchMove(event); break;
+        case 'touchend': this.onClick(event); break;
+        case 'click': this.onClick(event); break;
+    }
+};
+
+fastButton.prototype.onTouchStart = function(event) {
+    event.stopPropagation();
+    this.element.addEventListener('touchend', this, false);
+	document.body.addEventListener('touchmove', this, false);
+    this.startX = event.touches[0].clientX;
+    this.startY = event.touches[0].clientY;
+    this.element.style.backgroundColor = "rgba(0,0,0,.7)";
+};
+
+fastButton.prototype.onTouchMove = function(event) {
+    if(Math.abs(event.touches[0].clientX - this.startX) > 10 || Math.abs(event.touches[0].clientY - this.startY) > 10) {
+		this.reset();
+    }
+};
+fastButton.prototype.onClick = function(event) {
+    event.stopPropagation();
+    this.reset();
+    this.handler(event);
+    if(event.type == 'touchend') {
+		preventGhostClick(this.startX, this.startY);
+    }
+    this.element.style.backgroundColor = "";
+};
+fastButton.prototype.reset = function() {
+    this.element.removeEventListener('touchend', this, false);
+    document.body.removeEventListener('touchmove', this, false);
+    this.element.style.backgroundColor = "";
+};
+function preventGhostClick(x, y) {
+    coordinates.push(x, y);
+    window.setTimeout(gpop, 2500);
+};
+function gpop() {
+	coordinates.splice(0, 2);
+};
+function gonClick(event) {
+	for(var i = 0; i < coordinates.length; i += 2) {
+		var x = coordinates[i];
+        var y = coordinates[i + 1];
+        if(Math.abs(event.clientX - x) < 25 && Math.abs(event.clientY - y) < 25) {
+			event.stopPropagation();
+            event.preventDefault();
+        }
+    }
+};
+document.addEventListener('click', gonClick, true);
+var coordinates = [];
+
+
+// iOS Startup Image
+function iosStrtSrn() {
+  var filename = navigator.platform === 'iPad' ? 'h/' : 'l/';
+  document.write('<link rel="apple-touch-startup-image" href="/images/' + filename + 'splash.png" />' );
+}
+
+
+// Autogrow
+(function( $ ){
+
+  $.fn.growingTextarea = function( options ) {
+	
+	var settings = {
+      'lineHeight' : 12
+    };
+	
+    return this.each(function() {
+		if ( options ) { 
+			$.extend( settings, options );
+		}
+		var $this = $(this);
+		var textLineHeight = $this.css("line-height");
+		
+		if (textLineHeight.indexOf("px") == -1) {
+			textLineHeight = settings.lineHeight;
+		} else {
+			textLineHeight = parseInt(textLineHeight, 10);
+		}
+		$this.css('overflow','hidden');
+		$this.keyup(function(e){
+			var newHeight = $this.attr("scrollHeight");
+			var currentHeight = $this.attr("clientHeight");
+			if (newHeight > currentHeight) {
+				$this.css('height', newHeight + 3 * textLineHeight + 'px');
+			}
+		});
+    });
+
+  };
+})( jQuery );
